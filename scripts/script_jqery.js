d4d82780e69c91255f18a1b6482fc33b3335459d
elhaddaji_Otmane <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-06-20 19:02:18 +0100

    Create script_jqery.js

---
+++ scripts/script_jqery.js
@@ -0,0 +1,16 @@
+// function verifier(){
+//     var tnom = $("#tnom").val();
+//     var tprenom = $("#tprenom").val();
+//     var tmail = $("#tnaiss").val();
+//     var male = $("#rM").val();
+//     var Femme = $("#rF").val();
+//     var regexnom=/^[a-z]+$/gmi;
+//     if(regexnom.test(tnom)){
+//         alert("hello");
+//     }
+// }
+
+
+$(document).ready(function(){
+    
+})
